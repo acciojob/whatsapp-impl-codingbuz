3cd7c3527d5be514f4d6477d933cfc2b4f6eafae
Upendra Kumar <124766145+codingbuz@users.noreply.github.com>
2023-06-24 21:42:36 +0530

    Update User.java

--- src/main/java/com/driver/User.java
+++ src/main/java/com/driver/User.java
@@ -1,16 +1,27 @@
 package com.driver;
 
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-@AllArgsConstructor
 public class User {
     private String name;
     private String mobile;
 
-    public User(){}
+    public User(String name, String mobile) {
+        this.name = name;
+        this.mobile = mobile;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
 }
