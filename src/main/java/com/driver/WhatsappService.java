20a38f699e9b202e07c480fd826adebf36071d29
Upendra Kumar <124766145+codingbuz@users.noreply.github.com>
2023-06-24 21:39:40 +0530

    Update WhatsappService.java

--- src/main/java/com/driver/WhatsappService.java
+++ src/main/java/com/driver/WhatsappService.java
@@ -1,6 +1,5 @@
 package com.driver;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -8,29 +7,28 @@ import java.util.List;
 
 @Service
 public class WhatsappService {
-    
-    @Autowired
-    WhatsappRepository whatsappRepository;
-    public String createUser(String name, String mobile) {
-        whatsappRepository.createUser(name,mobile);
-        return "create user account";
-    }
 
-    public Group createGroup(List<User> users) {
-    }
+    WhatsappRepository whatsappRepository = new WhatsappRepository();
 
-    public int createMessage(String content) {
+    public String createUser(String name, String mobile) throws Exception {
+        return whatsappRepository.createUser(name,mobile);
     }
-
-    public int sendMessage(Message message, User sender, Group group) {
+    public Group createGroup(List<User> users){
+        return whatsappRepository.createGroup(users);
     }
-
-    public String changeAdmin(User approver, User user, Group group) {
+    public int createMessage(String content){
+        return whatsappRepository.createMessage(content);
     }
-
-    public int removeUser(User user) {
+    public int sendMessage(Message message, User sender, Group group) throws Exception{
+        return whatsappRepository.sendMessage(message,sender,group);
     }
-
-    public String findMessage(Date start, Date end, int k) {
+    public String changeAdmin(User approver, User user, Group group) throws Exception{
+        return whatsappRepository.changeAdmin(approver,user,group);
+    }
+    public int removeUser(User user) throws Exception{
+        return whatsappRepository.removeUser(user);
+    }
+    public String findMessage(Date start, Date end, int K) throws Exception{
+        return whatsappRepository.findMessage(start,end,K);
     }
 }
